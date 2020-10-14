f7e79b4bda696eacaa97c7103319a82d31ef5a18
Luiz Augusto Melo <luiz@twistereventos.com>
2020-10-14 14:39:02 -0300

    Adicionada mensagem de Bom dia

--- Section8/Program.cs
+++ Section8/Program.cs
@@ -7,6 +7,7 @@ namespace Section8
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Morning!");
 
             Console.ReadLine();
         }
